57a66294fdcd3b281e2ed6cab3a84ef144a95d2f
derick <derickcjesus@gmail.com>
2023-03-02 20:28:33 -0300

    Aviso caso o número falado seja válido

--- app/validacao.js
+++ app/validacao.js
@@ -1,6 +1,7 @@
 function verificarSeOChutePossuiUmValorValido(chute) {
 	const numero = +chute;
 
+	//Condição caso o número falado esteja inválido
 	if(chuteEhInvalido(numero)) {
 		elementoChute.innerHTML += '<div>Valor inválido</div>'
 	}
@@ -8,6 +9,14 @@ function verificarSeOChutePossuiUmValorValido(chute) {
 	if(numeroEhMaiorOuMenorQueOValorPermitido(numero)) {
 		elementoChute.innerHTML += `<div>Valor inválido: Fale 
 		um número entre ${menorValor} e ${maiorValor}</div>`
+	}
+
+	//Condição caso o número falado seja válido
+	if(numero === numeroAleatorio) {
+		document.body.innerHTML = `
+		<h2>Você acertou 🎉</h2>
+		<h3>O número secreto era ${numeroAleatorio}</h3>
+		`
 	}
 }
 
